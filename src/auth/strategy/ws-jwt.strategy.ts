66bc0bde12eb76bf8285366d7fc4bc0d0af86a79
Emre AKA <aka548548@gmail.com>
2022-12-07 20:54:02 +0300

    cookie

--- src/auth/strategy/ws-jwt.strategy.ts
+++ src/auth/strategy/ws-jwt.strategy.ts
@@ -1,15 +1,30 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { Request as RequestType } from 'express';
 
 @Injectable()
 export class WsJwtStrategy extends PassportStrategy(Strategy, 'wsjwt') {
     constructor() {
         super({
-            jwtFromRequest: ExtractJwt.fromUrlQueryParameter('bearerToken'),
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                WsJwtStrategy.extractJWT,
+                ExtractJwt.fromUrlQueryParameter('bearerToken'),
+            ]),
             secretOrKey: process.env.SECRET_KEY,
         })
     }
+
+    private static extractJWT(req: RequestType): string | null {
+        if (
+          req.cookies &&
+          'token' in req.cookies &&
+          req.cookies.user_token.length > 0
+        ) {
+          return req.cookies.token;
+        }
+        return null;
+      }
 
     async validate(payload: any) {
         return { userId: payload.sub, email: payload.email };
