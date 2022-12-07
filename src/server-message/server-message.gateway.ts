0a4e52df53c23e3a14c02664e94a0bf269744292
Emre AKA <aka548548@gmail.com>
2022-12-07 14:00:30 +0300

    ws

--- src/server-message/server-message.gateway.ts
+++ src/server-message/server-message.gateway.ts
@@ -1,6 +1,15 @@
+import { UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+    // transports: ['polling'],
+    // cors: {
+    //     origin: ['http://localhost:4200'],
+    //     credentials: true
+    // }
+})
 export class ServerMessageGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     handleDisconnect(client: any) {
     }
